1070a4a5ed32b12a2424e2606439a05d6ae1907e
gpsp95 <gabrielpspiedade@gmail.com>
2022-04-09 11:38:30 -0300

    primeiro teste

---
+++ preen_auto.py
@@ -0,0 +1,29 @@
+from fpdf import FPDF
+
+
+arquivo = []
+pos = []
+vazio = '*'
+'''
+nome = input('Digite o nome:  ')
+RG = input('Digite o RG: ')
+CPF = input('Digite o CPF:  ')
+
+texto_info = [nome, RG, CPF]
+
+'''
+ 
+
+
+
+
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size =15)
+
+
+pdf.cell(200, 10, txt = 'Hello World', ln = 3, align='C')
+    
+
+pdf.output(f"preenchido.pdf")
